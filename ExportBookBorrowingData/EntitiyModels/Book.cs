a0736221d90d8828b17a0722169ff58369805e88
Shroudzz <1833549764@qq.com>
2020-05-03 15:25:59 +0800

    添加实体

---
+++ ExportBookBorrowingData/EntitiyModels/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportBookBorrowingData.EntitiyModels
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string SeriesTitle { get; set; }
+        public string SeriesAuthor { get; set; }
+        public double Price { get; set; }
+        public string ISBN { get; set; }
+        public string Press { get; set; }
+        public string PublishYear { get; set; }
+        public string Pages { get; set; }
+        public string Version { get; set; }
+        public string Category { get; set; }
+        public string Classification { get; set; }
+
+        public Book()
+        {
+
+        }
+    }
+}
---
+++ ExportBookBorrowingData/EntitiyModels/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportBookBorrowingData.EntitiyModels
+{
+    public class Teacher
+    {
+        public string SerialNumber { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public Teacher()
+        {
+
+        }
+    }
+}
